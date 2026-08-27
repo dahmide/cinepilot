2f9b5660951983ce61b50383dd1c80726ffe5694
Dahmide <89408854+dahmide@users.noreply.github.com>
2026-08-27 14:58:18 +0100

    Fix type definition for IssuesPage params

--- frontend/app/app/projects/[id]/(dl)/issues/page.tsx
+++ frontend/app/app/projects/[id]/(dl)/issues/page.tsx
@@ -3,7 +3,7 @@ import { getIssues } from "@/lib/dal/issues.dal";
 
 export default async function IssuesPage({
     params
-}: Readonly<{ params: Promise<{ id }> }>) {
+}: Readonly<{ params: Promise<{ id: string }> }>) {
     const { id } = await params;
     const issues = await getIssues(id);
 
